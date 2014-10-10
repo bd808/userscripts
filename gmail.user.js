0462680e097abdf218fc74a829246de350972245
Bryan Davis <bd808@wikimedia.org>
2014-10-10 10:28:12 -0600

    Pretty things up and make script installable

--- gmail.user.js
+++ gmail.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         gmail hacks
 // @namespace    http://bd808.com/userscripts
-// @version      0.1
+// @version      0.2
 // @description  Make gmail look and work like I want it to
 // @author       Bryan Davis
 // @match        https://mail.google.com/*
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
-// @resource     gmailcss http://bd808.com/userscripts/gmail.css
-// @downloadURL  http://bd808.com/userscripts/gmail.js
-// @updateURL    http://bd808.com/userscripts/gmail.js
+// @resource     gmailcss http://bd808.com/userscripts/gmail.user.css
+// @downloadURL  http://bd808.com/userscripts/gmail.user.js
+// @updateURL    http://bd808.com/userscripts/gmail.user.js
 // ==/UserScript==
 GM_addStyle(GM_getResourceText('gmailcss'));
