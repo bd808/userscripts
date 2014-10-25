d4609f000ae00190557289068de433bb5f3088d8
Bryan Davis <bd808@bd808.com>
2014-10-25 16:59:22 -0600

    Stop hiding phui-timeline-minor-event

--- phabricator.user.js
+++ phabricator.user.js
@@ -4,7 +4,7 @@
 // @description  Make phabricator look and work like I want it to
 // @match        https://secure.phabricator.com/*
 // @match        https://phabricator.wikimedia.org/*
-// @version      0.1
+// @version      0.2
 // @author       Bryan Davis
 // @license      MiT License; http://opensource.org/licenses/MIT
 // @downloadURL  http://bd808.com/userscripts/phabricator.user.js
