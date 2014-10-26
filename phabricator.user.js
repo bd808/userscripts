9ac8964cdfd86d28471872b719e1c4bf93fe899f
Bryan Davis <bd808@bd808.com>
2014-10-25 19:13:01 -0600

    phabricator: hide some minor timeline events
    Events like "[user] created this task" and "[user] added
    a subscriber: [user]" are not important in my opinion. By hiding them
    I can see a bit more valuable content per vertical screen. Some minor
    events are actually valuable however so rather than my previous
    hide-them-all-with-css solution I'll use a bit of javascript to walk the
    dom and selectively hide events.

--- phabricator.user.js
+++ phabricator.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         phabricator hacks
-// @namespace    http://bd808.com/userscripts
+// @namespace    http://bd808.com/userscripts/
 // @description  Make phabricator look and work like I want it to
 // @match        https://secure.phabricator.com/*
 // @match        https://phabricator.wikimedia.org/*
-// @version      0.2
+// @version      0.3
 // @author       Bryan Davis
 // @license      MiT License; http://opensource.org/licenses/MIT
 // @downloadURL  http://bd808.com/userscripts/phabricator.user.js
@@ -13,4 +13,35 @@
 // @grant        GM_getResourceText
 // @resource     css http://bd808.com/userscripts/phabricator.user.css
 // ==/UserScript==
+
+/* Load custom css */
 GM_addStyle(GM_getResourceText('css'));
+
+/* Hide spammy minor events */
+(function() {
+    "use strict";
+    var nodes = document.querySelectorAll('.phui-timeline-minor-event'),
+        nodesLen = nodes.length,
+        spam = /created this task|added (a )?(subscriber|project)|changed Security from|moved this task to|awarded a token/;
+    for (var nodeIdx = 0; nodeIdx < nodesLen; nodeIdx++) {
+        var kids = nodes[nodeIdx].querySelectorAll('.phui-timeline-title'),
+            kidsLen = kids.length,
+            firstVisible = kidsLen + 1;
+        for (var kidIdx = 0; kidIdx < kidsLen; kidIdx++) {
+            if (kids[kidIdx].innerHTML.match(spam)) {
+                kids[kidIdx].style.display = 'none';
+            } else {
+                firstVisible = Math.min(firstVisible, kidIdx);
+            }
+        }
+        if (firstVisible < kidsLen) {
+            /* Not all items hidden, so move the timestamp to the first
+             * visible item in the list.
+             */
+            var extra = kids[0].querySelector('.phui-timeline-extra');
+            kids[firstVisible].appendChild(extra);
+        }
+    }
+})();
+
+/* vim:sw=4:ts=4:sts=4:et: */
