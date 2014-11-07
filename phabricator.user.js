cceae91010db2fbfb6f8ce1077efe9f8f6b1a046
Bryan Davis <bd808@wikimedia.org>
2014-11-07 16:07:27 -0700

    phabricator: Only hide minor events

--- phabricator.user.js
+++ phabricator.user.js
@@ -5,7 +5,7 @@
 // @match        https://secure.phabricator.com/*
 // @match        https://phabricator.wikimedia.org/*
 // @match        https://bugzillapreview.wmflabs.org/*
-// @version      0.7
+// @version      0.8
 // @author       Bryan Davis
 // @license      MiT License; http://opensource.org/licenses/MIT
 // @downloadURL  http://bd808.com/userscripts/phabricator.user.js
@@ -21,28 +21,28 @@ GM_addStyle(GM_getResourceText('css'));
 /* Hide spammy events */
 (function() {
     "use strict";
-    var nodes = document.querySelectorAll('.phui-timeline-event-view'),
+    var nodes = document.querySelectorAll('.phui-timeline-minor-event'),
         nodesLen = nodes.length,
-        spam = /created this task|(added|removed) (a )?(subscriber|project)|(changed|set) Security (from|to)|moved this task to|awarded a token|(raised|lowered) the priority of this task/;
+        spam = /created this task|(added|removed) (a )?(subscriber|project)|(changed|set) Security (from|to)|moved this task to|awarded a token|(raised|lowered) the priority of this task|triaged this task as/;
     for (var nodeIdx = 0; nodeIdx < nodesLen; nodeIdx++) {
         var kids = nodes[nodeIdx].querySelectorAll('.phui-timeline-title'),
             kidsLen = kids.length,
             firstVisible = kidsLen + 1;
         for (var kidIdx = 0; kidIdx < kidsLen; kidIdx++) {
             if (kids[kidIdx].innerHTML.match(spam)) {
+                // console.log('Hiding : ' + kids[kidIdx].outerHTML);
                 kids[kidIdx].style.display = 'none';
             } else {
                 firstVisible = Math.min(firstVisible, kidIdx);
             }
         }
         if (firstVisible < kidsLen) {
-            /* Not all items hidden, so move the timestamp to the first
-             * visible item in the list.
-             */
+            // Not all items hidden, so move the timestamp to the first
+            // visible item in the list.
             var extra = kids[0].querySelector('.phui-timeline-extra');
             kids[firstVisible].appendChild(extra);
         } else {
-            /* All items ended up hidden, so hide the whole minor event */
+            // All items ended up hidden, so hide the whole minor event
             nodes[nodeIdx].style.display = 'none';
         }
     }
