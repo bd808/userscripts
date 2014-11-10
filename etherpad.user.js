8975e5f6bfe428c33249e6c4aaf11eb319e7f9d0
Bryan Davis <bd808@wikimedia.org>
2014-11-10 12:16:31 -0700

    etherpad: Fix color
    base3, not base03.

--- etherpad.user.js
+++ etherpad.user.js
@@ -4,14 +4,13 @@
 // @description  Don't use this unless you are bd808!
 // @match        http://etherpad.*/p/*
 // @match        https://etherpad.*/p/*
-// @version      0.1
+// @version      0.2
 // @author       Bryan Davis
 // @license      MIT License; http://opensource.org/licenses/MIT
 // @downloadURL  http://bd808.com/userscripts/etherpad.user.js
 // @updateURL    http://bd808.com/userscripts/etherpad.user.js
 // ==/UserScript==
 (function() {
-    var base03 = '#002b36';
-    pad.notifyChangeColor(base03);
+    pad.notifyChangeColor('#fdf6e3');
     pad.notifyChangeName('bd808');
 })();
