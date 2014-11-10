bcee395da65aff4ca035cb9ba565aa3487c73dec
Bryan Davis <bd808@wikimedia.org>
2014-11-10 12:18:53 -0700

    etherpad: grant and run-at settings

--- etherpad.user.js
+++ etherpad.user.js
@@ -4,11 +4,13 @@
 // @description  Don't use this unless you are bd808!
 // @match        http://etherpad.*/p/*
 // @match        https://etherpad.*/p/*
-// @version      0.2
+// @version      0.3
 // @author       Bryan Davis
 // @license      MIT License; http://opensource.org/licenses/MIT
 // @downloadURL  http://bd808.com/userscripts/etherpad.user.js
 // @updateURL    http://bd808.com/userscripts/etherpad.user.js
+// @grant none
+// @run-at document-end
 // ==/UserScript==
 (function() {
     pad.notifyChangeColor('#fdf6e3');
