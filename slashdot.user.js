ede88269ff6ece219d1538bac45aa1c92477a0ee
Bryan Davis <bd808@bd808.com>
2015-01-14 20:09:28 -0700

    slashdot

---
+++ slashdot.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         slashdot hacks
+// @namespace    http://bd808.com/userscripts
+// @description  Make slashdot look and work like I want it to
+// @match        http://slashdot.org/*
+// @version      0.1
+// @author       Bryan Davis
+// @license      MiT License; http://opensource.org/licenses/MIT
+// @downloadURL  http://bd808.com/userscripts/slashdot.user.js
+// @updateURL    http://bd808.com/userscripts/slashdot.user.js
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @resource     slashdotcss http://bd808.com/userscripts/slashdot.user.css
+// ==/UserScript==
+GM_addStyle(GM_getResourceText('slashdotcss'));
