80a721223bb50c18823d3f356a8bcd5e03202941
Bryan Davis <bd808@wikimedia.org>
2016-04-21 10:52:43 -0600

    wmfgerrit: sideload https://github.com/jdlrobson/gerrit-be-nice-to-me

--- wmfgerrit.user.js
+++ wmfgerrit.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://bd808.com/userscripts/
 // @description  Make gerrit look and work like I want it to
 // @match        https://gerrit.wikimedia.org/*
-// @version      0.1
+// @version      0.2
 // @author       Bryan Davis
 // @license      MiT License; http://opensource.org/licenses/MIT
 // @downloadURL  https://bd808.github.io/userscripts/gerrit.user.js
@@ -11,5 +11,7 @@
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
 // @resource     gerritcss http://bd808.com/userscripts/wmfgerrit.user.css
+// @require      https://tools-static.wmflabs.org/cdnjs/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require      https://raw.githubusercontent.com/jdlrobson/gerrit-be-nice-to-me/master/main.js
 // ==/UserScript==
 GM_addStyle(GM_getResourceText('gerritcss'));
