41cbafa9741785fa2b73a86ed909159e6ebc7929
Bryan Davis <bd808@wikimedia.org>
2016-04-28 15:57:06 -0600

    phabricator: I just can't stop myself from piddling

--- phabricator.user.js
+++ phabricator.user.js
@@ -5,7 +5,7 @@
 // @match        https://secure.phabricator.com/*
 // @match        https://phabricator.wikimedia.org/*
 // @match        https://bugzillapreview.wmflabs.org/*
-// @version      0.25
+// @version      0.26
 // @author       Bryan Davis
 // @license      MiT License; http://opensource.org/licenses/MIT
 // @downloadURL  https://bd808.github.io/userscripts/phabricator.user.js
