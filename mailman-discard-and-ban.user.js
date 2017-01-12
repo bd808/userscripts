83b7d06f7214095bbfab23273a99cf81df41d2de
Bryan Davis <bd808@wikimedia.org>
2017-01-12 09:26:58 -0800

    Make pruning mailman held message queues quicker

---
+++ mailman-discard-and-ban.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name Mailman discard
+// @namespace http://bd808.com/userscripts/
+// @description Automatically selects "Discard" and ticks the "Add" and "Discards" checkboxes on Mailman admin requests for pending messages
+// @match *://*/mailman/admindb/*
+// @match *://*/lists/admindb/*
+// @version 0.1
+// @author Bryan Davis
+// @license MIT License; http://opensource.org/licenses/MIT
+// @downloadURL https://bd808.github.io/userscripts/mailman-discard-and-ban.user.js
+// @updateURL https://bd808.github.io/userscripts/mailman-discard-and-ban.user.js
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+(function() {
+    var inputs = document.getElementsByTagName('input'),
+        len = inputs.length;
+    for (var i = 0; i < len; i++) {
+        var input = inputs[i];
+        if (
+            input.name.toLowerCase().match(/^senderaction/) &&
+            input.value == '3'
+           ) {
+            input.checked = true;
+        } else if (input.name.toLowerCase().match(/^senderfilterp/)) {
+            input.checked = true;
+        }
+    }
+})();
