a0ed66fb7601167403798636ead030d32d09ca6a
Bryan Davis <bd808@wikimedia.org>
2018-02-15 15:14:32 -0700

    gcal: fix the damn task drawer click target

---
+++ gcal.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         gcal hacks
+// @namespace    http://bd808.com/userscripts/
+// @description  Make gcal look and work like I want it to
+// @match        https://calendar.google.com/*
+// @version      0.2
+// @author       Bryan Davis
+// @license      MIT License; http://opensource.org/licenses/MIT
+// @downloadURL  https://bd808.github.io/userscripts/gcal.user.js
+// @updateURL    https://bd808.github.io/userscripts/gcal.user.js
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @grant        GM.addStyle
+// @grant        GM.getResourceText
+// @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @resource     gcalcss https://bd808.github.io/userscripts/gcal.user.css
+// ==/UserScript==
+GM.addStyle(GM.getResourceText('gcalcss'));
