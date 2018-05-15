617de58b6fb99a3eabc27f361d8c8f573c7f27eb
Bryan Davis <bd808@wikimedia.org>
2018-05-15 12:13:38 -0600

    Gcal: more compact top nav

--- gcal.user.js
+++ gcal.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://bd808.com/userscripts/
 // @description  Make gcal look and work like I want it to
 // @match        https://calendar.google.com/*
-// @version      0.3
+// @version      0.4
 // @author       Bryan Davis
 // @license      MIT License; http://opensource.org/licenses/MIT
 // @downloadURL  https://bd808.github.io/userscripts/gcal.user.js
@@ -12,7 +12,8 @@
 // @grant        GM_getResourceText
 // @grant        GM.addStyle
 // @grant        GM.getResourceText
-// @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @grant        GM.getResourceUrl
+// @require      https://bd808.github.io/gm4-polyfill/gm4-polyfill.js
 // @resource     gcalcss https://bd808.github.io/userscripts/gcal.user.css
 // ==/UserScript==
 GM.getResourceText('gcalcss').then(function(css){GM.addStyle(css);});
