de562a15c1899f8a25723d63c17f0d7bfdfe361f
Bryan Davis <bd808@wikimedia.org>
2020-05-26 08:42:17 -0600

    etherpad: Add user css
    Adding some css to reduce padding and font size in "new" etherpad-lite
    skin.

--- etherpad.user.js
+++ etherpad.user.js
@@ -2,15 +2,21 @@
 // @name         etherpad personal settings
 // @namespace    http://bd808.com/userscripts/
 // @description  Don't use this unless you are bd808!
-// @match        *://etherpad.wikimedia.org/p/*
-// @version      0.5
+// @match        https://etherpad.wikimedia.org/p/*
+// @version      0.6
 // @author       Bryan Davis
 // @license      MIT License; http://opensource.org/licenses/MIT
 // @downloadURL  https://bd808.github.io/userscripts/etherpad.user.js
 // @updateURL    https://bd808.github.io/userscripts/etherpad.user.js
-// @grant none
-// @run-at document-end
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @grant        GM.addStyle
+// @grant        GM.getResourceText
+// @grant        GM.getResourceUrl
+// @require      https://bd808.github.io/gm4-polyfill/gm4-polyfill.js
+// @resource     etherpadcss https://bd808.github.io/userscripts/etherpad.user.css
 // ==/UserScript==
+GM.getResourceText('etherpadcss').then(function(css){GM.addStyle(css);});
 (function() {
     "use strict";
     var interval = window.setInterval(
