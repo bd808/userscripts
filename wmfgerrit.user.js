5e887e3b11e1ce259390fcfeab885fd303d1a5af
Bryan Davis <bd808@wikimedia.org>
2020-06-29 11:44:08 -0600

    wmfgerrit: tweak applied styles
    Set script to run in the document-idle phase to see if this makes styles
    actually show in the browser. May have to get more fancy with this as
    the UI seems to be at least partially drawn using JS which appears to
    mess with the ability of Greasemonkey to style it.
    
    Also try more forcefully to suppress the text site id.

--- wmfgerrit.user.js
+++ wmfgerrit.user.js
@@ -4,7 +4,7 @@
 // @description  Make gerrit look and work like I want it to
 // @match        https://gerrit.wikimedia.org/*
 // @match        https://gerrit-test.wikimedia.org/*
-// @version      20200625.1
+// @version      20200629.1
 // @author       Bryan Davis
 // @license      MIT License; http://opensource.org/licenses/MIT
 // @downloadURL  https://bd808.com/userscripts/wmfgerrit.user.js
@@ -16,5 +16,6 @@
 // @grant        GM.getResourceUrl
 // @require      https://bd808.com/gm4-polyfill/gm4-polyfill.js
 // @resource     css https://bd808.com/userscripts/wmfgerrit.user.css
+// @run-at       document-idle
 // ==/UserScript==
 GM.getResourceText('css').then(function(css){GM.addStyle(css);});
