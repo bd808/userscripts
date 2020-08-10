0771d3bad187cff218c4925a54b87f5c711dfb41
Bryan Davis <bd808@wikimedia.org>
2020-08-10 14:19:13 -0600

    phabricator: fix anchor scroll and highlight target
    Our DOM hiding steps mess up the browser's calculations of where an
    anchor target (location.hash) ends up vertically in the document. Hack
    around this by scrolling the element into view. Also add a css class to
    the target so we can style it and its children if desired.

--- phabricator.user.js
+++ phabricator.user.js
@@ -7,7 +7,7 @@
 // @match        https://bugzillapreview.wmflabs.org/*
 // @match        http://phabricator-striker.wmflabs.org/*
 // @match        https://phabricator-striker.wmflabs.org/*
-// @version      0.40.1
+// @version      20200810.04
 // @author       Bryan Davis
 // @license      MiT License; http://opensource.org/licenses/MIT
 // @downloadURL  https://bd808.com/userscripts/phabricator.user.js
@@ -37,36 +37,52 @@ GM.getResourceText('css')
     });
 
 /* Hide spammy events */
-var hideSpammyEvents = function() {
-    "use strict";
-    var nodes = document.querySelectorAll('.phui-timeline-minor-event'),
-        nodesLen = nodes.length,
-        spam = /(added|removed) (a )?(subscriber|project)|(changed|set) Security (from|to)|moved this task to|awarded a token|(raised|lowered) the priority of this task|triaged this task as|changed the title from/;
-    for (var nodeIdx = 0; nodeIdx < nodesLen; nodeIdx++) {
-        var kids = nodes[nodeIdx].querySelectorAll('.phui-timeline-title'),
-            kidsLen = kids.length,
-            firstVisible = kidsLen + 1;
-        for (var kidIdx = 0; kidIdx < kidsLen; kidIdx++) {
-            if (kids[kidIdx].innerHTML.match(spam)) {
-                // console.log('Hiding : ' + kids[kidIdx].outerHTML);
-                kids[kidIdx].style.display = 'none';
+var scrollAndHighlight = function(event) {
+        "use strict";
+        var activeClass = "bd808-active-hash",
+            hash = location.hash,
+            hashTarget = document.querySelector("#anchor-" + hash.substring(1)),
+            active = document.getElementsByClassName(activeClass);
+        for (var i = 0; i < active.length; i++) {
+            active[i].classList.remove(activeClass);
+        }
+        if (hash) {
+            hashTarget.classList.add(activeClass);
+            hashTarget.scrollIntoView();
+        }
+    },
+    hideSpammyEvents = function() {
+        "use strict";
+        var nodes = document.querySelectorAll('.phui-timeline-minor-event'),
+            nodesLen = nodes.length,
+            spam = /(added|removed) (a )?(subscriber|project)|(changed|set) Security (from|to)|moved this task to|awarded a token|(raised|lowered) the priority of this task|triaged this task as|changed the title from/;
+        for (var nodeIdx = 0; nodeIdx < nodesLen; nodeIdx++) {
+            var kids = nodes[nodeIdx].querySelectorAll('.phui-timeline-title'),
+                kidsLen = kids.length,
+                firstVisible = kidsLen + 1;
+            for (var kidIdx = 0; kidIdx < kidsLen; kidIdx++) {
+                if (kids[kidIdx].innerHTML.match(spam)) {
+                    // console.log('Hiding : ' + kids[kidIdx].outerHTML);
+                    kids[kidIdx].style.display = 'none';
+                } else {
+                    firstVisible = Math.min(firstVisible, kidIdx);
+                }
+            }
+            if (firstVisible < kidsLen) {
+                // Not all items hidden, so move the timestamp to the first
+                // visible item in the list.
+                var extra = kids[0].querySelector('.phui-timeline-extra');
+                kids[firstVisible].appendChild(extra);
             } else {
-                firstVisible = Math.min(firstVisible, kidIdx);
+                // All items ended up hidden, so hide the whole minor event
+                nodes[nodeIdx].style.display = 'none';
             }
         }
-        if (firstVisible < kidsLen) {
-            // Not all items hidden, so move the timestamp to the first
-            // visible item in the list.
-            var extra = kids[0].querySelector('.phui-timeline-extra');
-            kids[firstVisible].appendChild(extra);
-        } else {
-            // All items ended up hidden, so hide the whole minor event
-            nodes[nodeIdx].style.display = 'none';
-        }
-    }
-}
+        scrollAndHighlight();
+    };
 /* Run on page load */
 hideSpammyEvents();
+window.addEventListener("hashchange", scrollAndHighlight, false);
 
 /* Run when new nodes are inserted in the DOM too (Show Older Changes) */
 var mutationTarget = document.querySelector('.phui-timeline-view'),
