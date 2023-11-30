60be292723dd294e17746e877fae6bab4f97881a
Bryan Davis <bd808@wikimedia.org>
2023-11-29 17:18:07 -0700

    phabricator: Add MatmaRex's awesome gerritbot css
    * Add css from
      <https://userstyles.world/style/13140/wikimedia-phabricator-reduce-gerritbot-comments>
      to style gerritbot interactions compactly.
    * Add similar css to style CodeReviewBot interactions compactly.

--- phabricator.user.js
+++ phabricator.user.js
@@ -8,7 +8,7 @@
 // @match        https://bugzillapreview.wmflabs.org/*
 // @match        http://phabricator-striker.wmflabs.org/*
 // @match        https://phabricator-striker.wmflabs.org/*
-// @version      20230824.01
+// @version      20231129.01
 // @author       Bryan Davis
 // @license      MiT License; http://opensource.org/licenses/MIT
 // @downloadURL  https://bd808.com/userscripts/phabricator.user.js
